c7454219838e3916569f7b1d25af5da9ea39f465
ackarlse <ann.c.karlsen@hiof.no>
2023-02-24 10:22:13 +0100

    søkefunksjon, time 1

--- src/App.js
+++ src/App.js
@@ -9,18 +9,17 @@ import { useEffect, useState } from 'react'
 
 function App() {
   const [recipes, setRecipes] = useState([])
+  const [search, setSearch] = useState('chicken')
 
   const getRecipes = async() =>{
     //Pass på at ID og KEY kommer fra riktig app i edmam, hvis dere har registrert at dere skal bruke recipe api så må app id og key være koblet til det og ikke food databse eller omvendt
-    const response = await fetch('https://api.edamam.com/search?q=american&app_id=DIN_APP_ID&app_key=DIN_APP_KEY')
+    const response = await fetch(`https://api.edamam.com/search?q=${search}&app_id=52a150ec&app_key=ced7b20160d07136b580dc435e358b9f`)
     const data = await response.json()
     console.log(data.hits)
     setRecipes(data.hits)
 
   
   }
-  console.log("sjekk", recipes)
-
   useEffect(() =>{
     getRecipes()
   },[])
@@ -29,7 +28,7 @@ function App() {
   return (
       <Routes>
         <Route element={<Layout/>}>
-          <Route index element={<Main recipes={recipes} />} />
+          <Route index element={<Main recipes={recipes} setSearch={setSearch} getRecipes={getRecipes} />} />
           <Route path=':slug' element={<RecipePage recipes={recipes} />}/>
         </Route>
       </Routes>
